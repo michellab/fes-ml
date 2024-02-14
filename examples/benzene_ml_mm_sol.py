3609d71ea5ee03c63cb569346d6855b62affb97c
Joao Morado <joaomorado@gmail.com>
2024-02-14 15:04:42 +0000

    Added benzene examples

---
+++ examples/benzene_ml_mm_sol.py
@@ -0,0 +1,55 @@
+"""
+ML(sol)->MM(sol) free energy calculation for benzene in solution using an ML/MM approach.
+
+This script demonstrates how to perform a free energy calculation for benzene in solution using a ML/MM approach.
+The solute is alchemically modified using a lambda schedule that interpolates between ML and MM potentials.
+At lambda_emle=1, the solute is simulated with the ML potential in the electrostatic embedding scheme.
+At lambda_emle=0, the full system is simulated with the MM force field.
+
+Authors: Joao Morado
+"""
+if __name__ == "__main__":
+    import numpy as np
+
+    from fes_ml.fes import FES
+    from fes_ml.utils import plot_lambda_schedule
+
+    # Set up the alchemical modifications
+    n_lambda_emle = 10
+    emle_windows = np.linspace(1.0, 0.0, n_lambda_emle)
+
+    lambda_schedule = {"lambda_emle": emle_windows}
+
+    plot_lambda_schedule(lambda_schedule)
+
+    # Define the dynamics and EMLE parameters
+    dynamics_kwargs = {
+        "timestep": "1fs",
+        "cutoff_type": "pme",
+        "cutoff": "12A",
+        "integrator": "langevin_middle",
+        "temperature": "298.15K",
+        "platform": "reference",
+        "constraint": "h-bonds",
+    }
+
+    emle_kwargs = None
+
+    # Create the FES object to run the simulations
+    fes = FES(
+        top_file="/Users/admin/workspace/git_repos/fes-ml/data/benzene/benzene_sol.prmtop",
+        crd_file="/Users/admin/workspace/git_repos/fes-ml/data/benzene/benzene_sol.inpcrd",
+    )
+
+    # Create the alchemical states
+    fes.create_alchemical_states(
+        alchemical_atoms=list(range(12)),
+        lambda_schedule=lambda_schedule,
+        dynamics_kwargs=dynamics_kwargs,
+        emle_kwargs=emle_kwargs,
+    )
+
+    # Minimize the system and equilibrate it during 1 ns
+    fes.run_equilibration_batch(1000000, minimize=False)
+    # Sample 1000 times every ps (i.e. 1 ns of simulation per state)
+    fes.run_simulation_batch(1000, 1000)
---
+++ examples/benzene_mm_sol.py
@@ -0,0 +1,62 @@
+"""
+MM(sol)->MM(gas) free energy calculation for benzene.
+
+This script demonstrates how to calculate a direct absolute hydration free energy at the MM level.
+The solute is alchemically modified using a lambda schedule that decouples the solute from the solvent.
+At lambda_q=1, the solute-solvent electrostatic interactions are fully turned on.
+At lambda_q=0, the solute-solvent electrostatic interactions are fully turned off.
+At lambda_lj=1, the solute-solvent van der Waals interactions are fully turned on.
+At lambda_lj=0, the solute-solvent van der Waals interactions are fully turned off.
+
+Authors: Joao Morado
+"""
+if __name__ == "__main__":
+    import numpy as np
+
+    from fes_ml.fes import FES
+    from fes_ml.utils import plot_lambda_schedule
+
+    # Set up the alchemical modifications
+    n_lambda_q = 3
+    n_lambda_lj = 8
+    q_windows = np.linspace(1.0, 0.0, n_lambda_q, endpoint=False)
+    lj_windows = np.linspace(1.0, 0.0, n_lambda_lj)
+
+    lambda_schedule = {
+        "lambda_q": list(q_windows) + [0.0] * n_lambda_lj,
+        "lambda_lj": [1.0] * n_lambda_q + list(lj_windows),
+    }
+
+    plot_lambda_schedule(lambda_schedule)
+
+    # Define the dynamics and EMLE parameters
+    dynamics_kwargs = {
+        "timestep": "1fs",
+        "cutoff_type": "pme",
+        "cutoff": "12A",
+        "integrator": "langevin_middle",
+        "temperature": "298.15K",
+        "platform": "reference",
+        "constraint": "h-bonds",
+    }
+
+    emle_kwargs = None
+
+    # Create the FES object to run the simulations
+    fes = FES(
+        top_file="/Users/admin/workspace/git_repos/fes-ml/data/benzene/benzene_sol.prmtop",
+        crd_file="/Users/admin/workspace/git_repos/fes-ml/data/benzene/benzene_sol.inpcrd",
+    )
+
+    # Create the alchemical states
+    fes.create_alchemical_states(
+        alchemical_atoms=list(range(12)),
+        lambda_schedule=lambda_schedule,
+        dynamics_kwargs=dynamics_kwargs,
+        emle_kwargs=emle_kwargs,
+    )
+
+    # Minimize the system and equilibrate it during 1 ns
+    fes.run_equilibration_batch(1000000, minimize=False)
+    # Sample 1000 times every ps (i.e. 1 ns of simulation per state)
+    fes.run_simulation_batch(1000, 1000)
