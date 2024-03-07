bc73726c0fd6fe2cb27e397812c3e8c272cf3664
Joao Morado <joaomorado@gmail.com>
2024-03-07 11:20:20 +0000

    Added example of how to use electrostatic embedding at the MM level

---
+++ examples/benzene_mm_sol_emle.py
@@ -0,0 +1,76 @@
+"""
+MM(sol, electrostatic) -> MM(gas) free energy calculation for benzene.
+
+This script demonstrates how to calculate the absolute hydration free energy at the MM level in an electrostatic embedding scheme.
+The solute is alchemically modified using a lambda schedule that decouples it from the solvent.
+
+At lambda_emle=1, the solute-solvent electrostatic interactions are fully turned on (electrostatic embedding).
+At lambda_emle=0, the solute-solvent electrostatic interactions are fully turned off (mechanical embedding).
+At lambda_q=1, the solute-solvent electrostatic interactions are fully turned on (mechanical embedding).
+At lambda_q=0, the solute-solvent electrostatic interactions are fully turned off (mechanical embedding).
+At lambda_lj=1, the solute-solvent van der Waals interactions are fully turned on.
+At lambda_lj=0, the solute-solvent van der Waals interactions are fully turned off.
+
+Authors: Joao Morado
+"""
+if __name__ == "__main__":
+    import numpy as np
+
+    from fes_ml.fes import FES
+    from fes_ml.utils import plot_lambda_schedule
+
+    # Set up the alchemical modifications
+    n_lambda_emle = 6
+    n_lambda_q = 5
+    n_lambda_lj = 11
+    emle_windows = np.linspace(1.0, 0.0, n_lambda_emle)
+    q_windows = np.linspace(1.0, 0.0, n_lambda_q, endpoint=False)
+    lj_windows = np.linspace(1.0, 0.0, n_lambda_lj)
+
+    lambda_schedule = {
+        "lambda_emle": list(emle_windows) + [None] * (n_lambda_q + n_lambda_lj - 1),
+        "lambda_q": [None] * (n_lambda_emle - 1)
+        + list(q_windows)
+        + [0.0] * n_lambda_lj,
+        "lambda_lj": [None] * (n_lambda_emle - 1)
+        + [1.0] * n_lambda_q
+        + list(lj_windows),
+    }
+    plot_lambda_schedule(lambda_schedule)
+
+    # Define the dynamics and EMLE parameters
+    dynamics_kwargs = {
+        "timestep": "1fs",
+        "cutoff_type": "PME",
+        "cutoff": "12A",
+        "constraint": "h_bonds",
+        "integrator": "langevin_middle",
+        "temperature": "298.15K",
+        "pressure": "1atm",
+        "platform": "Reference",
+        "map": {"use_dispersion_correction": True, "tolerance": 0.0005},
+    }
+
+    emle_kwargs = {"method": "electrostatic", "backend": "sander", "device": "cpu"}
+
+    # Create the FES object to run the simulations
+    fes = FES(
+        top_file="../data/benzene/benzene_sol_sage.prmtop",
+        crd_file="../data/benzene/benzene_sol_sage.inpcrd",
+    )
+
+    # Create the alchemical states
+    fes.create_alchemical_states(
+        alchemical_atoms=list(range(12)),
+        lambda_schedule=lambda_schedule,
+        dynamics_kwargs=dynamics_kwargs,
+        emle_kwargs=emle_kwargs,
+    )
+
+    # Minimize
+    fes.run_minimization_batch(1000)
+    # Equilibrate during 1 ns
+    fes.run_equilibration_batch(1000000)
+    # Sample 1000 times every ps (i.e. 1 ns of simulation per state)
+    U_kln = fes.run_production_batch(1000, 1000)
+    np.save("U_kln_mm_sol.npy", np.asarray(U_kln))
